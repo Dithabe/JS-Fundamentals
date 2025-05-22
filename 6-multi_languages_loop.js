a702e1d41c663062d9a9f95e58042b5fa89d6579
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-22 22:14:29 +0200

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,5 +1,3 @@
 for (let index = 0; index < 1 ; index++) {
-    console.log("C is fun");
-    console.log("Python is cool");
-    console.log("JavaScript is amazing");  
+    console.log("C is fun \nPython is cool \nJavaScript is amazing");
 }
