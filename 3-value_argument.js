a6a296ba27e67d8505a156b195d4ef96f5b0cef4
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-22 21:27:24 +0200

    Update 3-value_argument.js

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,6 +1,6 @@
 const arguments = process.argv;  //declaring the imput array to be used
 
 if (arguments[2])  //conditional statement checking that an input is provided at index 2
-  console.log(arguments);
+  console.log(arguments[2]);
 else
-  cosnsole.log('No argument');
+  console.log('No argument');
