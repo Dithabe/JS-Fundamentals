1b854a24804564b31df5f3c0ac34269f1062fba4
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-22 22:33:33 +0200

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,6 +1,7 @@
 const args = process.argv;
 
-for (let index = 0 ; index < Number(args[2]) ; index++)
-  console.log("C is fun");
-
-console.log("Missing number of occurrences");
+if (Number(args[2]))
+    for (let index = 0 ; index < Number(args[2]) ; index++)
+        console.log("C is fun");
+else 
+    console.log("Missing number of occurrences");
