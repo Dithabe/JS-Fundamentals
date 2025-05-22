c2b8aeb36231944a8e1176cf38532013ae088ff0
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-22 22:02:30 +0200

    Create 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,6 @@
+const arg = process.argv;  //initiating the argument vector 
+
+if (Number(args[2])    //casting the input to an integer
+    console.log("My number: " + arg[2]);
+else                    //catering for non-number integers
+  console.log("Not a number");
