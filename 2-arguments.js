c444d55d565225ffda50e2274652f5bbb2da6753
Dithabe <61557057+Dithabe@users.noreply.github.com>
2025-05-22 20:36:16 +0200

    Create 2-arguments.js

---
+++ 2-arguments.js
@@ -0,0 +1,8 @@
+const arguments = process.argv;
+
+if (arguments.length < 1)
+    console.log('No argument');
+else if (arguments == 1)
+    console.log("Argument found");
+else if (arguments == 2)
+    console.log("Arguments found");
